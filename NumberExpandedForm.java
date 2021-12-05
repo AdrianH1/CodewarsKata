0274873eaaff8acbca72a9862a8d48a8c22bcf8e
AdrianHugentobler <adrian.hugentobler@hotmail.com>
2021-12-05 08:05:49 +0100

    Solution + Best Practice

--- NumberExpandedForm.java
+++ NumberExpandedForm.java
@@ -4,42 +4,50 @@ public class NumberExpandedForm {
         System.out.println(expandedForm(42)); //40 + 2
         System.out.println(expandedForm(70304)); //70000 + 300 + 4
         System.out.println(expandedForm(9000000)); //900000
+
+        System.out.println(bestPracticeExpandedForm(12)); //10 + 2
+        System.out.println(bestPracticeExpandedForm(42)); //40 + 2
+        System.out.println(bestPracticeExpandedForm(70304)); //70000 + 300 + 4
+        System.out.println(bestPracticeExpandedForm(9000000)); //900000
     }
 
     public static String expandedForm(int num) {
-        int power = String.valueOf(num).length()-1;
         StringBuilder sb = new StringBuilder();
-        while (power >= 0){
-            int pow = (int) Math.pow(10, power); //10000
-            int multiplier = num/pow; //7
-            if (multiplier != 0){
-                sb.append(String.valueOf(multiplier * pow) + " + ");
+        int length = String.valueOf(num).length();
+        int power = String.valueOf(num).length()-1;
+        while (length > 0){
+            int powOfTen = (int) Math.pow(10, power);
+            int multiplier = num/powOfTen;
+            if (String.valueOf(num).charAt(0) != '0'){
+                sb.append(String.valueOf(multiplier * powOfTen));
             }
-            if (String.valueOf(num).length() == 1){
+            num = num - (multiplier * powOfTen);
+            if (num != 0){
+                sb.append(" + ");
+            }
+            if (num > 0 && num < 10){
                 sb.append(String.valueOf(num));
+                break;
+            } else if (num == 0) {
+                break;
+            } else {
+                length = String.valueOf(num).length();
+                power = String.valueOf(num).length()-1;
             }
-            num = num - (multiplier * pow); //304
-            power--;
         }
         return sb.toString();
     }
 
-    // public static String expandedForm(int num) {
-    //     String number = String.valueOf(num);
-    //     int zeros = number.length()-1;
-    //     StringBuilder sb = new StringBuilder();
-    //     for (int i = 0; i < number.length(); i++){
-    //         if (number.charAt(i) != '0'){
-    //             sb.append(number.charAt(i)).toString();
-    //             for(int j = 0; j < zeros; j++){
-    //                 sb.append("0").toString();
-    //             }
-    //             if (i != number.length()-1){
-    //                 sb.append(" + ");
-    //             }
-    //         }
-    //         zeros--;
-    //     }
-    //     return sb.toString();
-    // }
+    public static String bestPracticeExpandedForm(int num)
+    {
+        String outs = "";
+        for (int i = 10; i < num; i *= 10) {
+            int rem = num % i;
+            outs = (rem > 0) ? " + " + rem + outs : outs;
+            num -= rem;
+        }
+        outs = num + outs;
+        
+        return outs;
+    }
 }
